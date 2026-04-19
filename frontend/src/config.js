5a6de4c635172ef0bd62de830c3d3941bb671674
Abishanan Sriranjan <abishanan.sriran@gmail.com>
2026-04-19 02:17:07 -0400

    update api url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const isDev = window.location.hostname === "localhost";
 
 export const API_URL = isDev
   ? "http://localhost:3001"   // dev backend
-  : window.location.origin;         // production backend
+  : "https://api.abi.gg";         // production backend
 
 export const RECAPTCHA_SITE_KEY = isDev
   ? import.meta.env.VITE_RECAPTCHA_TEST_SITE_KEY
