784904bc2fd8b2d12d9a31e7f32a7ed4f8cf2f12
Abishanan Sriranjan <abishanan.sriran@gmail.com>
2026-04-29 04:07:06 -0400

    optimize vercel deploy to frontend/ diffs

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
-  # Proceed with the build
-  echo "✅ - Build can proceed"
-  exit 1;
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
+  git diff --name-only HEAD^ HEAD | grep -qE '^frontend/'
+  if [ $? -eq 0 ]; then
+    echo "✅ - Build can proceed"
+    exit 1
+  else
+    echo "🛑 - No frontend changes, build cancelled"
+    exit 0
+  fi
 else
-  # Don't build
   echo "🛑 - Build cancelled"
-  exit 0;
+  exit 0
 fi
