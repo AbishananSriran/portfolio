6dbbf14e1364cfb3c6c37023062aa0cb4b783ed9
Abishanan Sriranjan <abishanan.sriran@gmail.com>
2026-04-29 03:43:30 -0400

    fix aws deploy script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,12 +3,11 @@
 
 # Before running script, sign in to AWS CLI
 # using `aws configure` and provide your credentials.
-# Ensure you are at root of project.
+# Ensure you are at backend folder of project.
 
 npm ci && \
 git archive HEAD --format=zip -o code.zip . && \
 zip -r code.zip node_modules/ && \
-cd .. && \
 aws lambda update-function-code \
     --function-name backend \
     --zip-file fileb://code.zip
